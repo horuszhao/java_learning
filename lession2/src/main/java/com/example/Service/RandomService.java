1a7fccc19f9ff10b1968880f3793e8aa639710f5
Terry <zhaoyun413@163.com>
2017-10-10 20:56:33 +0800

    创建RestControll 并输出随机数组和生成随机数组的起始时间、结束时间以及执行耗时

---
+++ lession2/src/main/java/com/example/Service/RandomService.java
@@ -0,0 +1,13 @@
+package com.example.Service;
+
+
+import com.example.Model.RandomResultModel;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Beyondhost on 10/10/2017.
+ */
+
+public interface RandomService {
+    RandomResultModel getRandomArray();
+}
