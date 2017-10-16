c048d016204eb4e44b0104fcdb9f16d5c3a26d57
Terry <zhaoyun413@163.com>
2017-10-16 21:29:16 +0800

    2）通过Bean Validation验证参数不能为空，数字不能大于100同时不能小于50等条件

--- Week5/src/test/java/MyBastisTest.java
+++ Week5/src/test/java/MyBastisTest.java
@@ -3,6 +3,10 @@ import com.mybatisstudy.domain.User;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import java.util.*;
 
 public class MyBastisTest {
@@ -16,7 +20,7 @@ public class MyBastisTest {
     }
 
     /**
-     *  1）通过Mybatis批量插入N条数据到数据库
+     * 1）通过Mybatis批量插入N条数据到数据库
      */
     @Test
     public void batchInsertUser() {
@@ -31,6 +35,25 @@ public class MyBastisTest {
         UserService service = new UserService();
         int r = service.batchInsert(users);
         System.out.println(r);
+        Assert.assertTrue(true);
+    }
+
+    /**
+     * 2）通过Bean Validation验证参数不能为空，数字不能大于100同时不能小于50等条件
+     */
+    @Test
+    public void beanValidation() {
+        User user = new User();
+        user.setName("a");
+        user.setAge(10 + 100);
+
+        ValidatorFactory vf = Validation.buildDefaultValidatorFactory();
+        Validator validator = vf.getValidator();
+
+        Set<ConstraintViolation<User>> validateResult = validator.validate(user);
+        for (ConstraintViolation<User> cv : validateResult) {
+            System.out.println(cv.getMessage() + "(" + cv.getPropertyPath() + ")" + "-->" + cv.getInvalidValue());
+        }
         Assert.assertTrue(true);
     }
 }
